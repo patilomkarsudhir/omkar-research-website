331c6d5c89407606c707b02c2927504f2ade5838
Patil <patilomkarsudhir@ufl.edu>
2026-02-17 15:41:55 -0500

    fix: remove magnifying glass icon from genealogy tree

--- app/fun-stuff/fun-stuff-client.tsx
+++ app/fun-stuff/fun-stuff-client.tsx
@@ -25,11 +25,6 @@ export default function FunStuffClient() {
                 height={200}
                 className="rounded"
               />
-              <div className="absolute top-2 right-2 bg-white/20 hover:bg-white/30 rounded-full p-2 transition-colors">
-                <svg className="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM13 13H9" />
-                </svg>
-              </div>
             </div>
           </div>
         </div>
